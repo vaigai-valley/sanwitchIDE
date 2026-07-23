0a37ab24245ee61036f77165c6545d6b1371a292
mohan k.vengatesan <64476232+Mkvenkatesan@users.noreply.github.com>
2026-07-23 22:55:31 +0530

    Update Sanwitch IDE project: main.py

--- sanwitch-projects/main.py
+++ sanwitch-projects/main.py
@@ -1,1 +1,7 @@
-# Generated MicroPython code will appear here
+from machine import Pin, PWM
+
+Pin(2, Pin.OUT).value(1)
+PWM(Pin(2), freq=1000, duty=512)
+_buzz = PWM(Pin(15), freq=2000, duty=512)
+time.sleep_ms(200)
+_buzz.deinit()
